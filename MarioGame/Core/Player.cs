aa8e5b4c99b08362d3f489759778fd262b8048a4
Dmitry <dimakureev@mail.ru>
2025-01-05 18:02:04 +0700

    add top collision

--- MarioGame/Core/Player.cs
+++ MarioGame/Core/Player.cs
@@ -84,36 +84,54 @@ public class Player
         {
             return;
         }
-        
+
         // Левая граница
-        if(X < 0)
+        if (X < 0)
         {
             X = 0;
             _velocityX = 0;
         }
 
         // Проверка столкновения с землей
+        // TODO: переместить в функции взаимодействия с объектом?
         var isBlockOnDirectionMove = false;
         _isOnGround = false;
         foreach (var obj in objects)
         {
-            if (obj is GroundObject ground && IsCollidingWithGround(ground))
+            if (obj is GroundObject ground && IsCollidingWithBlockOnMoveY(ground, _jumpVelocity))
             {
-                _isOnGround = true;
-                Y = ground.Y - Height;
-                if (_velocityX > 0)
+                //                _isOnGround = true;
+                if (_isOnGround)
+                {
+                    Y = ground.Y - Height;
+
+                    if (_velocityX > 0)
+                        _playerStatus = PlayerStatus.IsMovingRight;
+                    else if (_velocityX < 0)
+                        _playerStatus = PlayerStatus.IsMovingLeft;
+                    else
+                        _playerStatus = PlayerStatus.Idle;
+                }
+                else
+                {
+                    _jumpVelocity = 0;
+                    Y = ground.Y + ground.Height;
+                }
+
+/*                if (_velocityX > 0)
                     _playerStatus = PlayerStatus.IsMovingRight;
                 else if (_velocityX < 0)
                     _playerStatus = PlayerStatus.IsMovingLeft;
                 else
-                    _playerStatus = PlayerStatus.Idle;
+                    _playerStatus = PlayerStatus.Idle;*/
                 break;
             }
         }
 
-        foreach(var obj in objects)
+        // Коллизия с объектами на пути перемещения
+        foreach (var obj in objects)
         {
-            if (obj is GroundObject groundOnMove && IsCollidingWithBlockOnMove(groundOnMove, _velocityX))
+            if (obj is GroundObject groundOnMove && IsCollidingWithBlockOnMoveX(groundOnMove, _velocityX))
             {
                 X = _velocityX > 0 ? groundOnMove.X - Width : groundOnMove.X + groundOnMove.Width;
                 isBlockOnDirectionMove = true;
@@ -142,22 +160,43 @@ public class Player
 
         return false;
     }
-    
+
     public void OnDeath()
     {
         PlayerDied?.Invoke();
     }
 
-    private bool IsCollidingWithGround(GroundObject ground)
+/*    private bool IsCollidingWithGround(GroundObject ground)
     {
         return Y + Height >= ground.Y && X + Width > ground.X && X < ground.X + ground.Width;
-    }
-    
-    private bool IsCollidingWithBlockOnMove(GroundObject ground, double shift)
+    }*/
+
+    private bool IsCollidingWithBlockOnMoveX(GroundObject ground, double shift)
     {
         return Y + Height > ground.Y && Y < ground.Y + ground.Height &&
               (X + shift + Width > ground.X && X + shift + Width < ground.X + ground.Width ||
                X + shift > ground.X && X + shift < ground.X + ground.Width);
+    }
 
+    private bool IsCollidingWithBlockOnMoveY(GroundObject ground, double shift)
+    {
+        var isCollisionY = false;
+
+        if (X + Width > ground.X && X < ground.X + ground.Width)
+        {
+            if (Y + shift > ground.Y && Y + shift < ground.Y + ground.Height)
+                isCollisionY = true;
+
+            if (Y + shift + Height > ground.Y && Y + shift + Height < ground.Y + ground.Height)
+            {
+                isCollisionY = true;
+                _isOnGround = true;
+            }
+        }
+
+        return isCollisionY;
+        /*        return Y + Height > ground.Y && Y < ground.Y + ground.Height &&
+                      (X + shift + Width > ground.X && X + shift + Width < ground.X + ground.Width ||
+                       X + shift > ground.X && X + shift < ground.X + ground.Width);*/
     }
 }
