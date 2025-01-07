5937c4858d5ef5e353ca10cae5e5a6987d08ff8f
ilyavtln <owtf086@gmail.com>
2025-01-08 04:53:49 +0700

    remove

--- MarioGame/Core/GameManager.cs
+++ MarioGame/Core/GameManager.cs
@@ -95,7 +95,6 @@ public class GameManager
     {
         _gameStatus = GameStatus.Playing;
         _gameTime = TimeSpan.Zero;
-        //_level?.DrawLevel();
         _isRunning = true;
 
         var player = _level?.GetPlayer();
@@ -121,8 +120,6 @@ public class GameManager
         
         if (_gameStatus == GameStatus.Playing && player != null)
         {
-            //_canvas.Children.Clear();
-
             _level?.Update();
             
             _gameTime += deltaTime;
