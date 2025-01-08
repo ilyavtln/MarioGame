4cf1e29e82040831994ca2d64768720b1ed76622
owtf086 <owtf086@gmail.com>
2025-01-08 17:19:49 +0300

    New imgs

--- MarioGame/Config/GameConfig.cs
+++ MarioGame/Config/GameConfig.cs
@@ -4,6 +4,7 @@ public static class GameConfig
 {
     public const int LevelDuration = 15;
     public const int Lives = 3;
+    public const int TubeWidth = 96;
     public const int Fps = 60;
     public static readonly TimeSpan FrameInterval = TimeSpan.FromSeconds(1.0 / Fps);
 }
